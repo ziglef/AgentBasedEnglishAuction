730e2538554c1ad423ba49b9117ea0025e7b2135
Rui Rocha <ruigrandaorocha@gmail.com>
2015-04-19 18:34:49 +0100

    Added simple JFrame

--- src/gui/UserInterface.java
+++ src/gui/UserInterface.java
@@ -1,8 +1,22 @@
 package gui;
 
+import javax.swing.*;
+
 public class UserInterface {
 
     public static void main(String [] args) {
-        System.out.println("Hello world!");
+
+        JFrame window = new JFrame("TNEL - English Auction");
+        JPanel mainPanel = new JPanel(true);
+        JLabel testLabel = new JLabel("Hello World");
+
+        mainPanel.add(testLabel);
+        window.add(mainPanel);
+
+        window.setSize(640, 480);
+        window.setVisible(true);
+        window.setLocationRelativeTo(null);
+        window.validate();
+        window.repaint();
     }
 }
