3f6571c2b8278c6e00758a882048bd05444862cf
André Silva <andre.ee11304@gmail.com>
2015-05-14 18:41:01 +0100

    Created application component xml file and programmatically added the application to platform.

--- src/gui/UserInterface.java
+++ src/gui/UserInterface.java
@@ -1,27 +1,34 @@
 package gui;
 
 import jadex.base.Starter;
+import jadex.bridge.IComponentIdentifier;
 import jadex.bridge.IExternalAccess;
-import jadex.commons.future.IFuture;
+import jadex.bridge.service.RequiredServiceInfo;
+import jadex.bridge.service.search.SServiceProvider;
+import jadex.bridge.service.types.cms.IComponentManagementService;
 import jadex.commons.future.ThreadSuspendable;
 
 public class UserInterface {
 
     public static void main(String [] args) {
 
-        IExternalAccess platform;
-
         String[] defargs = new String[] {
                 "-gui", "true",
                 "-welcome", "false",
                 "-cli", "false",
                 "-printpass", "false",
-                "-platformname", "TNEL - English Auction"
+                "-platformname", "TNEL"
         };
-        IFuture<IExternalAccess> platfut = Starter.createPlatform(defargs);
 
         ThreadSuspendable sus = new ThreadSuspendable();
-        platform = platfut.get(sus);
+
+        IExternalAccess platform = Starter.createPlatform(defargs).get(sus);
         System.out.println("Started platform: "+platform.getComponentIdentifier());
+
+        IComponentManagementService cms = SServiceProvider.getService(platform.getServiceProvider(),
+                IComponentManagementService.class, RequiredServiceInfo.SCOPE_PLATFORM).get(sus);
+
+        IComponentIdentifier cid = cms.createComponent("application/EnglishAuction.application.xml", null).getFirstResult(sus);
+        System.out.println("CID: "+cid);
     }
 }
