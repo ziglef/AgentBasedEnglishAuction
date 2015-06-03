c9d1438cf791ebef57df533f220925f5b0ddbd9c
André Silva <andre.ee11304@gmail.com>
2015-06-03 01:41:34 +0100

    Added BidderBDI.

--- src/agents/BidderBDI.java
+++ src/agents/BidderBDI.java
@@ -1,7 +1,12 @@
 package agents;
 
+import jadex.bdiv3.BDIAgent;
+import jadex.bdiv3.annotation.Belief;
 import jadex.micro.annotation.Agent;
+import jadex.micro.annotation.AgentBody;
 import jadex.micro.annotation.Description;
+
+import java.util.List;
 
 /**
  * Created on 19/04/15.
@@ -13,4 +18,18 @@ import jadex.micro.annotation.Description;
 @Agent
 @Description("This agent represents a bidder")
 public class BidderBDI {
+
+    @Agent
+    protected BDIAgent bidder;
+
+    // List with the auctions the bidder is in
+    @Belief
+    protected List<String> auctions;
+
+    @AgentBody
+    public void body() {
+        bidder.waitForDelay(1000).get();
+
+        bidder.getComponentIdentifier().getLocalName();
+    }
 }
